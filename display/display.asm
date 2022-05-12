a17fb2bdb3721788bcf1ea00249d97074107c234
Matt Muldowney <matt.muldowney@gmail.com>
2022-05-12 15:56:35 -0700

    display stuff

--- display/display.asm
+++ display/display.asm
@@ -69,6 +69,528 @@
 
 .cseg
 
+; SetCursor(r, c, c1, c2)
+; =======================
+;
+; Description
+; -----------
+; Sets the pixel at row r, column c of the display grid to blink between
+; color c1 and color c2, switching every 500 ms. This indicates to the user
+; that the cursor is located at row r, column c. The possible colors are
+; blank, red, green, and yellow.
+;
+; Operational Description
+; -----------------------
+; Sets display[r, c] according to c1, cursorRed2 and cursorGreen2 according to 
+; c2, cursorRow to r, cursorColumn to c, such the the display multiplexor 
+; knows how to set up the blinking of the cursor location.
+;
+; Arguments
+; ---------
+; r (int, r16): row of the display (between 0 and 7)
+; c (int, r17): column of the display (between 0 and 7)
+; c1 (int, r18): first color for the cursor to blink (0: blank, 
+;   1: red, 2: green, 3: yellow)
+; c2 (int, r19): second color for the cursor to blink (0: blank, 
+;   1: red, 2: green, 3: yellow)
+;
+; Return Values
+; -------------
+; None
+;
+; Global Variables
+; ----------------
+; None
+;
+; Shared Variables
+; ----------------
+; cursorRow (int): W
+; cursorColumn (int): W
+; cursorRedColor1 (int): W
+; cursorGreenColor1 (int): W
+; cursorRedColor2 (int): W
+; cursorGreenColor2 (int): W
+;
+; Local Variables
+; ---------------
+; None
+;
+; Inputs
+; ------
+; None
+;
+; Outputs
+; -------
+; None
+;
+; Error Handling
+; --------------
+; if r or c is outside of 0 and 7:
+;   this is fine: in fact, we use this to "unset" the cursor (such that
+;   no pixel on the display is blinking)
+; if c1 or c2 is outside of 0 and 3:
+;   if they are above 3, they get reset to 3; if they are below 0, they get
+;   reset to 0.
+;
+; Algorithms
+; ----------
+; None
+;
+; Data Structures
+; ---------------
+; None
+;
+; Registers Used
+; --------------
+; r16: r
+; r17: c
+; r18: c1
+; r19: c2
+;
+; Stack Depth
+; -----------
+; 5 bytes
+;
+; Limitations
+; -----------
+; None
+;
+; Known Bugs
+; ----------
+; None
+;
+; Special Notes
+; -------------
+; None
+SetCursor:
+    ;;; arguments
+    .def r = r16    ; row
+    .def c = r17    ; column
+    .def c1 = r18   ; cursor color 1
+    .def c2 = r19   ; cursor color 2
+    
+    ;;; other registers we need
+    push r21        ; cursor red color 1
+    .def red1 = r21
+    push r22        ; cursor green color 1
+    .def green1 = r22
+    push r23        ; cursor red color 2
+    .def red2 = r23
+    push r24        ; cursor green color 2
+    .def green2 = r24
+
+    push r20        ; constant YELLOW
+    .def yellow = r20
+    ldi yellow, YELLOW
+
+
+    ;;; set the (row, column) position of the cursor
+    sts cursorRow, r
+    sts cursorColumn, c
+
+
+    ;;; saturate c1, c2 args if invalid (less than OFF, greater than YELLOW)
+    push r16
+    push r17
+    push r18
+    mov r16, c1
+    ldi r17, OFF
+    ldi r18, YELLOW
+    rcall Saturate
+    pop r18
+    pop r17
+    mov c1, r16
+    pop r16
+
+    push r16
+    push r17
+    push r18
+    mov r16, c2
+    ldi r17, OFF
+    ldi r18, YELLOW
+    rcall Saturate
+    pop r18
+    pop r17
+    mov c2, r16
+    pop r16
+
+
+    ;;; set the color information of the cursor
+  SetCursorColors:
+    ; red1 is bit0 of c1
+    mov red1, c1
+    andi red1, TRUE
+    sts cursorRedColor1, red1
+
+    ; green1 is bit 1 of c1
+    mov green1, c1
+    lsr green1
+    sts cursorGreenColor1, green1
+
+    ; red2 is bit0 of c2
+    mov red2, c2
+    andi red2, TRUE
+    sts cursorRedColor2, red2
+
+    ; green2 is bit 2 of c2
+    mov green2, c2
+    lsr green2
+    sts cursorGreenColor2, green2
+    
+    
+    ;;; return
+    pop r20
+    pop r24
+    pop r23
+    pop r22
+    pop r21
+    ret
+
+
+
+
+; ClearDisplay()
+; ==============
+;
+; Description
+; -----------
+; clears the LED display such that all LEDS are off
+; doesn't affect cursor
+;
+; Operational Description
+; -----------------------
+; Sets all columns in the buffer to OFF
+;
+; Arguments
+; ---------
+; None
+;
+; Return Values
+; -------------
+; None
+;
+; Global Variables
+; ----------------
+; None
+;
+; Shared Variables
+; ----------------
+; redBuffer (8 x 8 boolean matrix): W
+; greenBuffer (8 x 8 boolean matrix): W
+;
+; Local Variables
+; ---------------
+; None
+;
+; Inputs
+; ------
+; None
+;
+; Outputs
+; -------
+; None
+;
+; Error Handling
+; --------------
+; None
+;
+; Algorithms
+; ----------
+; None
+;
+; Data Structures
+; ---------------
+; None
+;
+; Registers Used
+; --------------
+; None
+;
+; Stack Depth
+; -----------
+; 2 bytes
+;
+; Limitations
+; -----------
+; None
+;
+; Known Bugs
+; ----------
+; None
+;
+; Special Notes
+; -------------
+; None
+ClearDisplay:
+    ;;; push needed registers
+    push r16        ; r16: OFF
+    .def off = r16
+    ldi off, OFF
+
+    push r17        ; r17: loop index
+    .def idx = r17
+
+    ; y: redBuffer, z: greenBuffer
+    ldi yl, low(redBuffer)
+    ldi yh, high(redBuffer)
+    ldi zl, low(greenBuffer)
+    ldi zh, high(greenBuffer)
+
+    ;;; turn off every column in red and green buffers
+    ldi idx, 0
+  ClearBuffersLoop:
+    st y+, off
+    st z+, off
+    cpi idx, NUM_COLS
+    brlt ClearBuffersLoop
+    
+
+    ;;; return
+    pop r17
+    pop r16
+    ret
+
+
+
+; PlotPixel(r, c, color)
+; ======================
+;
+; Description
+; -----------
+; Sets the pixel at row r, column c of the display grid to the specied color
+; (blank, red, green, or yellow).
+;
+; Operational Description
+; -----------------------
+; Sets display[r, c] according to color.
+;
+; Arguments
+; ---------
+; r (int, r16): row of the display (between 0 and 7)
+; c (int, r17): column of the display (between 0 and 7)
+; color (int, r18): color for pixel at r, c (0: blank, 1: red, 2: green, 
+;   3: yellow)
+;
+; Return Values
+; -------------
+; None
+;
+; Global Variables
+; ----------------
+; None
+;
+; Shared Variables
+; ----------------
+; redBuffer (8 x 8 bool matrix): W
+; greenBuffer (8 x 8 bool matrix): W
+;
+; Local Variables
+; ---------------
+; None
+;
+; Inputs
+; ------
+; None
+;
+; Outputs
+; -------
+; None
+;
+; Error Handling
+; --------------
+; if r or c is outside of 0 and 7, don't do anything (return right away)
+; if color is less than OFF set to OFF; if it is greater than YELLOW set
+;   to YELLOW
+;
+; Algorithms
+; ----------
+; None
+;
+; Data Structures
+; ---------------
+; None
+;
+; Registers Used
+; --------------
+; r16: r
+; r17: c
+; r18: color
+;
+; Stack Depth
+; -----------
+; None
+;
+; Limitations
+; -----------
+; None
+;
+; Known Bugs
+; ----------
+; None
+;
+; Special Notes
+; -------------
+; None
+;
+; Pseudocode
+; ----------
+; 
+; red = TRUE & color
+; red << r
+; redBuffer[c] |= red
+; 
+; green = color >> 1
+; green << r
+; greenBuffer[c] |= green
+PlotPixel:
+    ;;; arguments
+    .def r = r16        ; r16: display row
+    .def c = r17        ; r17: display column
+    .def color = r18    ; r18: color to set
+
+
+    ;;; other registers we need
+    push r19            ; r19: red color
+    .def red = r19
+
+    push r20            ; r20: green color
+    .def green = r20
+
+    push r22            ; r22: YELLOW
+    .def yellow = r22
+    ldi yellow, YELLOW
+
+    push r23            ; r23: redBuffer[c]
+    .def redBuffer_c = r23
+
+    push r24            ; r24: greenBuffer[c]
+    .def greenBuffer_c = r24
+
+    ;;; check validity of (r,c) , return if invalid
+    push r18
+    ldi r18, TRUE ; we can use this for each call since CheckValid only
+                    ; changes its value if invalid
+
+    ; r must be >= 0
+    push r16
+    push r17
+    mov r16, r
+    ldi r17, 0
+    rcall CheckValid
+    pop r17
+    pop r16
+
+    ; r must be < NUM_ROWS
+    push r16
+    push r17
+    ldi r16, NUM_ROWS
+    dec r16
+    mov r17, r
+    rcall CheckValid
+    pop r17
+    pop r16
+
+    ; c must be >= 0
+    push r16
+    push r17
+    mov r16, c
+    ldi r17, 0
+    rcall CheckValid
+    pop r17
+    pop r16
+
+    ; c must be < NUM_COLS
+    push r16
+    push r17
+    ldi r16, NUM_ROWS
+    dec r16
+    mov r17, r
+    rcall CheckValid
+    pop r17
+    pop r16
+
+    ; return if any of the above args are invalid
+    cpi r18, TRUE
+    pop r18
+    breq CheckColorValidity_PlotPixel
+    ret
+
+    ;;; check validity of color, set to nearest bound (OFF or YELLOW) if out
+    ;;; of bounds
+    ; color must be >= OFF
+    push r16
+    push r17
+    push r18
+    mov r16, color
+    ldi r17, OFF
+    ldi r18, YELLOW
+    rcall Saturate
+    pop r18
+    pop r17
+    mov color, r16
+    pop r16
+
+
+    ;;; set redBuffer[r, c]
+    mov red, color
+
+    ; get red bit from color
+    andi red, TRUE
+
+    ; red << r
+    push r16
+    push r17
+    ld r16, red
+    ld r17, r
+    rcall lslk
+    mov red, r16
+    pop r17
+    pop r16
+
+    ; put y at redBuffer[c]
+    ldi yl, low(redBuffer)
+    ldi yh, high(redBuffer)
+    add yl, c
+    adc yh, 0
+    ; get contents at redBuffer[c]
+    lds redBuffer_c, y
+
+    ; redBuffer[c] |= red
+    or redBuffer_c, red
+    sts y, redBuffer_c
+
+
+    ;;; set greenBuffer[r, c]
+    mov green, color
+
+    ; get green bit from color
+    andi green, TRUE
+
+    ; green << r
+    push r16
+    push r17
+    ld r16, green
+    ld r17, r
+    rcall lslk
+    mov green, r16
+    pop r17
+    pop r16
+
+    ; put y at greenBuffer[c]
+    ldi yl, low(greenBuffer)
+    ldi yh, high(greenBuffer)
+    add yl, c
+    adc yh, 0
+    ; get contents at greenBuffer[c]
+    lds greenBuffer_c, y
+
+    ; greenBuffer[c] |= green
+    or greenBuffer_c, green
+    sts y, greenBuffer_c
+
+    ret
+
+
 ; InitDisplayVars()
 ; =================
 ;
@@ -148,52 +670,46 @@
 ; -------------
 ; None
 InitDisplayVars:
-    ldi r16, COL_INIT
+    push r16        ; r16: multipurpose
+    .def temp = r16
 
-    ; init every column in redBuffer and greenBuffer
-    ldi yl, low(redBuffer)
-    ldi yh, high(redBuffer)
-    ldi zl, low(greenBuffer)
-    ldi zh, high(greenBuffer)
-    ldi r17, 0 ; index of loop
-    InitBuffers:
-        st y+, r16
-        st z+, r16
+    rcall ClearDisplay
 
-        inc r17
-        cpi r17, NUM_COLS
-        brlt InitBuffers
-
-    ; init buffer offset (for display mux) to 0
-    ldi r16, BUFF_OFFSET_INIT
-    sts ledBufferOffset, r16
+    ; init buffer offset (for display mux)
+    ldi temp, BUFF_OFFSET_INIT
+    sts ledBufferOffset, temp
     
     ; init column masks (for display mux)
-    ldi r16, COL_MASK_G_INIT
-    sts columnMaskG, r16
-    ldi r16, COL_MASK_R_INIT
-    sts columnMaskR, r16
+    ldi temp, COL_MASK_G_INIT
+    sts columnMaskG, temp
+    ldi temp, COL_MASK_R_INIT
+    sts columnMaskR, temp
     
     ; init cursor change counter
-    ldi r16, CURSOR_COUNTER_INIT
-    sts cursorChangeCounter, r16
+    ldi temp, CURSOR_COUNTER_INIT
+    sts cursorChangeCounter, temp
 
     ; init cursor blinking
-    ldi r16, USE_CURSOR_COLOR2_INIT
-    sts useCursorColor2, r16
+    ldi temp, USE_CURSOR_COLOR2_INIT
+    sts useCursorColor2, temp
 
     ; init cursor position
-    ldi r16, CURSOR_ROW_INIT
-    sts cursorRow, r16
-    ldi r16, CURSOR_COL_INIT
-    sts cursorColumn, r16
+    ldi temp, CURSOR_ROW_INIT
+    sts cursorRow, temp
+    ldi temp, CURSOR_COL_INIT
+    sts cursorColumn, temp
 
-    ; init alternate cursor color
-    ldi r16, CURSOR_RED2_INIT
-    sts cursorRed2, r16
-    ldi r16, CURSOR_GREEN2_INIT
-    sts cursorGreen2, r16
+    ; init cursor color
+    ldi temp, CURSOR_RED_COLOR_1_INIT
+    sts cursorRedColor1, temp
+    ldi temp, CURSOR_GREEN_COLOR_1_INIT
+    sts cursorGreenColor1, temp
+    ldi temp, CURSOR_RED_COLOR_2_INIT
+    sts cursorRedColor2, temp
+    ldi temp, CURSOR_GREEN_COLOR_2_INIT
+    sts cursorGreenColor2, temp
 
+    ret
 
 
 ; MultiplexDisplay()
@@ -328,9 +844,148 @@ InitDisplayVars:
 ;
 ; write back sreg
 MultiplexDisplay:
+    ; r16: temporary stuff
+    .def temp = r16
+    push temp
+
+    ; r17, r18: column masks
+    .def colMaskG = r17
+    push colMaskG
+    lds colMaskG, columnMaskG
+    .def colMaskR = r18
+    push colMaskR
+    lds colMaskR, columnMaskR
+
+    ; r19: buffer offset
+    .def bufferOffset = r19
+    push bufferOffset
+    lds bufferOffset, ledBufferOffset
+
+    ; r20, r21: cursor stuff
+    .def useColor2 = r20
+    push useColor2
+    lds useColor2, useCursorColor2
+    .def changeCounter = r21
+    push changeCounter
+    lds changeCounter, cursorChangeCounter
+    ; r22: the cursor color we are setting (could be for red/green 1/2 
+    ;   depending)
+    .def cursorColor = r22
+
+    ;;; turn off all columns
+    ldi temp, OFF
+    out porta, temp
+    out portd, temp
+
+    ;;; figure out if we want to load green or red into Y
+    ; if the 1 is in columnMaskG, load greenBuffer into Y
+    cpi colMaskG, 0
+    breq LoadRedBuffer
+    ldi yl, low(greenBuffer)
+    ldi yh, high(greenBuffer)
+
+    ; figure out which green color (1 or 2) we want for the cursor
+    cpi useColor2, FALSE
+    brne UseCursorGreenColor2
+    lds cursorColor, cursorGreenColor1
+    jmp ResolveCursorInBuffer
+  UseCursorGreenColor2:
+    lds cursorColor, cursorGreenColor2
+    ;jmp ResolveCursorInBuffer
+
+    ; otherwise the 1 is in columnMaskR, so load redBuffer into Y
+  LoadRedBuffer:
     ldi yl, low(redBuffer)
     ldi yh, high(redBuffer)
-    ldi zl, low(greenBuffer)
-    ldi zh, high(greenBuffer)
+
+    ; figure out which red color (1 or 2) we want for the cursor
+    cpi useColor2, FALSE
+    brne UseCursorRedColor2
+    lds cursorColor, cursorRedColor1
+    jmp ResolveCursorInBuffer
+  UseCursorRedColor2:
+    lds cursorColor, cursorRedColor2
+    ;jmp ResolveCursorInBuffer
+
+    ;;; figure out if we're at the cursor's column
+  ResolveCursorInBuffer:
+    ; adjust y by offset (so that we're at the right column)
+    add yl, bufferOffset
+    adc yh, 0
+
+    ; if the current column is the cursor's column ...
+    lds temp, cursorColumn
+    cpi bufferOffset, temp
+    lds temp, y ; put the column vector in temp
+    brne OutY2C
+    ; and if cursorColor is set ...
+    cpi cursorColor, TRUE
+    brne ClearCursorInBuffer
+    ; then set the current column's cursor row bit
+    ; note that r16 (temp) already contains the column vector
+    push r17
+    lds r17, cursorRow
+    rcall SetBit
+    pop r17
+    ; temp[r] (r16) is now set
+    jmp OutY2C
+  ClearCursorInBuffer:
+    ; otherwise clear the current column's cursor row bit
+    ; note that r16 (temp) already contains the column vector
+    push r17
+    lds r17, cursorRow
+    rcall SetBit
+    pop r17
+    ; temp[r] (r16) is now cleared
+    ;jmp OutY2C
+
+    ;;; output y (the rows of the current column) to port c
+  OutY2C:
+    out portc, temp
 
     
+    ;;; adjust cursor vars and re-init if necessary
+    dec changeCounter
+    cpi changeCounter, 0
+    brne ReInitOffset
+    ; if cursorChangeCounter is at 0 ...
+    ; invert useCursorColor2
+    eor useColor2, TRUE
+    ; reinit cursorChangeCounter
+    ldi changeCounter, CURSOR_COUNTER_INIT
+    sts cursorChangeCounter, changeCounter
+
+
+    ;;; inc led buffer offset (the indexer) and reinit if necessary
+    inc bufferOffset
+    cpi bufferOffset, NUM_COLS
+    brne StoreBufferOffset
+    ldi bufferOffset, BUFF_OFFSET_INIT
+  StoreBufferOffset:
+    sts ledBufferOffset, bufferOffset
+
+
+    ;;; output column masks to their respective ports
+    ; green -> port a
+    out porta, colMaskG
+    ; red -> port d
+    out portd, colMaskR
+
+    ;;; rotate column masks
+    cpi colMaskG, 0x00
+    brne RotateColumnMasks
+    cpi colMaskR, 0x01
+    brne RotateColumnMasks
+    ldi colMaskG, COL_MASK_G_INIT
+    ldi colMaskR, COL_MASK_R_INIT
+    jmp StoreColumnMasks
+
+  RotateColumnMasks:
+    lsr colMaskG
+    ror colMaskR
+
+  StoreColumnMasks:
+    sts columnMaskG, colMaskG
+    sts columnMaskR, colMaskR
+
+    ret
---
+++ util/util.asm
@@ -0,0 +1,592 @@
+; ClearBit(byte, bitNumber)
+; =======================
+;
+; Description
+; -----------
+; clears byte[bitNumber]
+;
+; Operational Description
+; -----------------------
+; ROLs 0b1111 1110 such that the 0 is in the bitNumber position, and then 
+;   ANDs it with byte to ensure byte[bitNumber] is cleared
+;
+; Arguments
+; ---------
+; byte (8-bit string, r16): the byte which has a bit we want to clear
+; bitNumber(int, r17): the position of the bit in the byte we want to clear;
+;   must be between 0 and 7
+;
+; Return Values
+; -------------
+; 8-bit string, r16: byte with byte[bitNumber] set 
+;
+; Global Variables
+; ----------------
+; None
+;
+; Shared Variables
+; ----------------
+; None
+;
+; Local Variables
+; ---------------
+; None
+;
+; Inputs
+; ------
+; None
+;
+; Outputs
+; -------
+; None
+;
+; Error Handling
+; --------------
+; if bitNumber is out of range [0, 7], do nothing and return
+;
+; Algorithms
+; ----------
+; None
+;
+; Data Structures
+; ---------------
+; None
+;
+; Registers Used
+; --------------
+; r16: byte
+; r17: bitNumber
+;
+; Stack Depth
+; -----------
+; 1 byte
+;
+; Limitations
+; -----------
+; None
+;
+; Special Notes
+; -------------
+; None
+ClearBit:
+    ;;; arguments
+    .def byte = r16
+    .def bitNumber = r17
+
+    ;;; check validity of bitNumber
+    ; bitNumber must be >= 0
+    push r18
+    ldi r18, TRUE
+    push r16
+    push r17
+    mov r16, bitNumber
+    ldi r17, 0
+    rcall CheckValid
+    pop r17
+    pop r16
+
+    ; bitNumber must be <= 7 (byte length)
+    push r16
+    push r17
+    ldi r16, 7
+    mov r17, bitNumber
+    rcall CheckValid
+    pop r17
+    pop r16
+
+    cpi r18, TRUE
+    breq ClearBitBody
+    ret
+
+
+  ClearBitBody:
+    ;;; other needed registers
+    ; r18: bit mask (this gets shifted appropriately)
+    .def mask = r18
+    push mask
+    ldi mask, 0b11111110
+
+    ;;; shift the bit mask left bitNumber times
+    push r16
+    push r17
+    mov r16, mask
+    ; set carry flag so that we rotate 1s back
+    sec
+    ; rolk(mask, bitNumber)
+    rcall rolk 
+    mov mask, r16
+    pop r17
+    pop r16
+
+    ;;; set the bit in byte
+    and byte, mask
+    pop mask
+    ret
+
+
+; SetBit(byte, bitNumber)
+; =======================
+;
+; Description
+; -----------
+; sets byte[bitNumber]
+;
+; Operational Description
+; -----------------------
+; LSLs 0x01 such that the 1 is in the bitNumber position, and then ORs it with
+; byte to ensure byte[bitNumber] is set
+;
+; Arguments
+; ---------
+; byte (8-bit string, r16): the byte which has a bit we want to set
+; bitNumber(int, r17): the position of the bit in the byte we want to set;
+;   must be between 0 and 7
+;
+; Return Values
+; -------------
+; 8-bit string, r16: byte with byte[bitNumber] set 
+;
+; Global Variables
+; ----------------
+; None
+;
+; Shared Variables
+; ----------------
+; None
+;
+; Local Variables
+; ---------------
+; None
+;
+; Inputs
+; ------
+; None
+;
+; Outputs
+; -------
+; None
+;
+; Error Handling
+; --------------
+; if bitNumber is out of range [0, 7], do nothing and return
+;
+; Algorithms
+; ----------
+; None
+;
+; Data Structures
+; ---------------
+; None
+;
+; Registers Used
+; --------------
+; r16: byte
+; r17: bitNumber
+;
+; Stack Depth
+; -----------
+; 1 byte
+;
+; Limitations
+; -----------
+; None
+;
+; Special Notes
+; -------------
+; None
+SetBit:
+    ;;; arguments
+    .def byte = r16
+    .def bitNumber = r17
+
+    ;;; check validity of bitNumber
+    ; bitNumber must be >= 0
+    push r18
+    ldi r18, TRUE
+    push r16
+    push r17
+    mov r16, bitNumber
+    ldi r17, 0
+    rcall CheckValid
+    pop r17
+    pop r16
+
+    ; bitNumber must be <= 7 (byte length)
+    push r16
+    push r17
+    ldi r16, 7
+    mov r17, bitNumber
+    rcall CheckValid
+    pop r17
+    pop r16
+
+    cpi r18, TRUE
+    breq SetBitBody
+    ret
+
+    ;;; other needed registers
+    ; r18: bit mask (this gets shifted appropriately)
+    .def mask = r18
+    push mask
+    ldi mask, 0x01
+
+    ;;; shift the bit mask left bitNumber times
+    push r16
+    push r17
+    mov r16, mask
+    ; mask << bitNumber (lslk(mask, bitNumber))
+    lslk 
+    mov mask, r16
+    pop r17
+    pop r16
+
+    ;;; set the bit in byte
+    or byte, mask
+    pop mask
+    ret
+
+
+; CheckValid(value1, value2, validFlag)
+; =====================================
+;
+; Description
+; -----------
+; sets validFlag to FALSE iff value1 < value2; otherwise, does nothing
+;
+; Operational Description
+; -----------------------
+; compares value1 and value2; if value1 >= value2, do nothing (just return);
+; if value1 < value2, this is consdered invalid, so set validFlag = FALSE
+;
+; Arguments
+; ---------
+; value1 (int, r16): value that must be greater than or equal to
+; value2 (int, r17): value that must be less than or equal to
+; validFlag (bool, r18): flag to set to false if invalid
+;
+; Return Values
+; -------------
+; bool, r18: flag indicating validity
+;
+; Global Variables
+; ----------------
+; None
+;
+; Shared Variables
+; ----------------
+; None
+;
+; Local Variables
+; ---------------
+; None
+;
+; Inputs
+; ------
+; None
+;
+; Outputs
+; -------
+; None
+;
+; Error Handling
+; --------------
+; None
+;
+; Algorithms
+; ----------
+; None
+;
+; Data Structures
+; ---------------
+; None
+;
+; Registers Used
+; --------------
+; r16: value1
+; r17: value2
+; r18: validFlag
+;
+; Stack Depth
+; -----------
+; None
+;
+; Limitations
+; -----------
+; None
+;
+; Special Notes
+; -------------
+; None
+CheckValid:
+    ;;; arguments
+    .def value1 = r16
+    .def value2 = r17
+    .def validFlag = r18
+
+    ;;; check that value1 >= value2
+    cp value1, value2
+    brge Return_CheckValid
+    ldi validFlag, FALSE
+
+  Return_CheckValid:
+    ret
+
+
+; Saturate(value, lowerBound, upperBound)
+; =======================================
+;
+; Description
+; -----------
+; if value < lowerBound, sets value = lowerBound; if value > upperBound,
+; value = upperBound; otherwise, value is untouched
+;
+; Operational Description
+; -----------------------
+; See description
+;
+; Arguments
+; ---------
+; value (int, r16): value that will possibly be saturated
+; lowerBound (int, r17): value that must be less than or equal to
+; upperBound (bool, r18): value that must be greater than or equal to
+;
+; Return Values
+; -------------
+; int, r16: the saturated value
+;
+; Global Variables
+; ----------------
+; None
+;
+; Shared Variables
+; ----------------
+; None
+;
+; Local Variables
+; ---------------
+; None
+;
+; Inputs
+; ------
+; None
+;
+; Outputs
+; -------
+; None
+;
+; Error Handling
+; --------------
+; None
+;
+; Algorithms
+; ----------
+; None
+;
+; Data Structures
+; ---------------
+; None
+;
+; Registers Used
+; --------------
+; r16: value
+; r17: lowerBound
+; r18: upperbound
+;
+; Stack Depth
+; -----------
+; None
+;
+; Limitations
+; -----------
+; None
+;
+; Special Notes
+; -------------
+; None
+Saturate:
+    ;;; arguments
+    .def value = r16
+    .def lowerBound = r17
+    .def upperBound = r18
+
+    ;;; check if value < lowerBound
+    cp value, lowerBound
+    brge CheckTooLarge_Saturate
+    ; saturate if it is
+    ldi value, lowerBound
+    jmp Return_Saturate
+
+    ;;; check if value > upperBound
+  CheckTooLarge:
+    cp upperBound, value
+    brge Return_Saturate
+    ; saturate if it is
+    ldi value, upperBound
+
+  Return_Saturate:
+    ret
+
+
+; lslk(byte, k)
+; ==================
+;
+; Description
+; -----------
+; logically shifts byte left k times
+;
+; Operational Description
+; -----------------------
+; performs k lsl's on byte
+;
+; Arguments
+; ---------
+; byte (8-bit string, r16): byte to lsl
+; k (int, r17): amount to lsl
+;
+; Return Values
+; -------------
+; 8-bit string, r16: byte << k
+;
+; Global Variables
+; ----------------
+; None
+;
+; Shared Variables
+; ----------------
+; None
+;
+; Local Variables
+; ---------------
+; None
+;
+; Inputs
+; ------
+; None
+;
+; Outputs
+; -------
+; None
+;
+; Error Handling
+; --------------
+; None
+;
+; Algorithms
+; ----------
+; None
+;
+; Data Structures
+; ---------------
+; None
+;
+; Registers Used
+; --------------
+; r16: byte
+; r17: k
+;
+; Stack Depth
+; -----------
+; None
+;
+; Limitations
+; -----------
+; None
+;
+; Special Notes
+; -------------
+; None
+lslk:
+    .def byte = r16 ; r16: the thing we are lsl-ing
+    .def k = r17    ; r17: the amount to lsl
+
+  lslLoop:
+    cpi     k, 0
+    breq    lslDone
+    lsl     byte
+    dec     k
+    jmp     lslLoop
+  lslDone:
+    ret
+
+
+; rolk(byte, k)
+; ==================
+;
+; Description
+; -----------
+; rotates byte left through carry k times
+;
+; Operational Description
+; -----------------------
+; performs k rol's on byte
+;
+; Arguments
+; ---------
+; byte (8-bit string, r16): byte to rol
+; k (int, r17): amount to rol
+;
+; Return Values
+; -------------
+; 8-bit string, r16: byte << k
+;
+; Global Variables
+; ----------------
+; None
+;
+; Shared Variables
+; ----------------
+; None
+;
+; Local Variables
+; ---------------
+; None
+;
+; Inputs
+; ------
+; None
+;
+; Outputs
+; -------
+; None
+;
+; Error Handling
+; --------------
+; None
+;
+; Algorithms
+; ----------
+; None
+;
+; Data Structures
+; ---------------
+; None
+;
+; Registers Used
+; --------------
+; r16: byte
+; r17: k
+;
+; Stack Depth
+; -----------
+; None
+;
+; Limitations
+; -----------
+; None
+;
+; Special Notes
+; -------------
+; None
+rolk:
+    ;;; arguments
+    .def byte = r16
+    .def k = r17
+
+  rolLoop:
+    cpi     k, 0
+    breq    rolDone
+    rol     byte
+    dec     k
+    jmp     rolLoop
+  rolDone:
+    ret
