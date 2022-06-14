3fb4dff072ff41b79c85571b5bf786176f77cf37
Matt Muldowney <matt.muldowney@gmail.com>
2022-06-13 19:47:42 -0700

    stuff

--- binario/AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "U:\mmuldown\ee010b\binario\labels.tmp" -fI -W+ie -o "U:\mmuldown\ee010b\binario\utiltest.hex" -d "U:\mmuldown\ee010b\binario\utiltest.obj" -e "U:\mmuldown\ee010b\binario\utiltest.eep" -m "U:\mmuldown\ee010b\binario\utiltest.map" "U:\mmuldown\ee010b\binario\hw2test.asm"
